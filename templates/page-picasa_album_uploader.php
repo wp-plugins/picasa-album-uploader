790814a3a3032d93951374a126edd421666761ac
draca <draca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-05 19:33:45 +0000

    Place plugin under revision control
    git-svn-id: https://plugins.svn.wordpress.org/picasa-album-uploader/trunk@190104 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/page-picasa_album_uploader.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package Picasa Album Uploader
+ * @author Kenneth J. Brucker <ken@pumastudios.com>
+ * @copyright 2010 Kenneth J. Brucker (email: ken@pumastudios.com)
+ * 
+ * This file is part of Picasa Album Uploader, a plugin for Wordpress.
+ *
+ * Picasa Album Uploader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Picasa Album Uploader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Picasa Album Uploader.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+  // Load header-picasa_album_uploader.php if available
+	get_header( 'picasa_album_uploader' );
+?>
+<div id="content" class="narrowcolumn" role="main">
+	<?php if (have_posts()) : ?>
+		
+		<?php while (have_posts()) : the_post(); ?>
+			<div class="post" id="post-<?php the_ID(); ?>">
+				<div class="entry">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<h2 class="center">Not Found</h2>
+		<p class="center">Sorry, but you are looking for something that isn't here.</p>
+
+	<?php endif; ?>
+</div> <!-- End Content -->
+<?php
+	// Load footer-picasa_album_uploader.php if available
+	get_footer( 'picasa_album_uploader' );
+?>
